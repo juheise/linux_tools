f248725337aba54517c79d470e1228125a2eb1fa
Julian Heise <julian.heise@zalando.de>
2018-11-27 14:23:22 +0100

    added validation

--- script/lib/console.py
+++ script/lib/console.py
@@ -3,11 +3,16 @@ import sys
 
 def progress(title, x):
     """
-    prints progress in %, overwriting the current line
+    prints progress in %, overwriting the current line. Values above 100 and below 0 will be clipped.
     Example:
         progress("counting", 50) # produces:
         counting: [=========================________________________](50.00%)
     """
+
+    if x < 0:
+        x = 0
+    if x > 100:
+        x = 100
 
     fif = int(x/2)
     inv = 49-fif
