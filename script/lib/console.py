6de6e43eb656ae777fb1835700d6121ab133368a
Julian Heise <julian.heise@zalando.de>
2018-11-27 14:19:34 +0100

    added docstring; renamed a file

--- script/lib/console.py
+++ script/lib/console.py
@@ -2,6 +2,13 @@ import sys
 
 
 def progress(title, x):
+    """
+    prints progress in %, overwriting the current line
+    Example:
+        progress("counting", 50) # produces:
+        counting: [=========================________________________](50.00%)
+    """
+
     fif = int(x/2)
     inv = 49-fif
     sys.stdout.write("\r" + title + ": [" + "="*fif + "_"*inv + "](" + "{0:.2f}".format(x) + "%)")
